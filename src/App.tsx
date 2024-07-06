b4b17f8f383085e1b1e97fa7c36c5a21d82802c3
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-07-07 01:43:33 +0500

    refactor: Update App component to use react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/pages/login/login-page";
 import SignupPage from "./components/pages/signup/signup-page";
 
 function App() {
-  return (
-    <main>
-      {/* <LoginPage /> */}
-      <SignupPage />
-    </main>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "signup",
+      element: <SignupPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
